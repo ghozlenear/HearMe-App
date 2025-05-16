be5338ace2653429bbd3e8c040045801189e6278
ramy <ze.mohramy@gmail.com>
2025-05-16 18:55:58 +0200

    done with auth , still patient creation to be done with express

--- app/lib/supabase.js
+++ app/lib/supabase.js
@@ -1,8 +1,8 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://your-project.supabase.co';
-const supabaseAnonKey = 'your-anon-key'; // Replace with your actual key
+const supabaseUrl = 'https://mqelzoakbtjajdduwjou.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1xZWx6b2FrYnRqYWpkZHV3am91Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcxODAwMjcsImV4cCI6MjA2Mjc1NjAyN30.hUL5kWrg4T7e62KjUklDFGYID4hoGQ6K75x8Aa2LbRA'; // Replace with your actual key
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
